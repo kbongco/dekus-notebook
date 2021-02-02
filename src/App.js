36e5ebd293f5a681f06887f8e4c58fce0c1a5bd5
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2021-02-02 15:23:46 -0500

    fixed up welcome page

--- src/App.js
+++ src/App.js
@@ -1,46 +1,53 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
-import './App.css';
-import axios from 'axios'
-import Welcome from './Screens/Welcome/Welcome'
-
+import "./App.css";
+import axios from "axios";
+import Welcome from "./Screens/Welcome/Welcome";
 
 function App() {
-  const [students, setStudents] = useState([])
-  const [proHeroes, setProHeroes] = useState([])
-  const [villains, setVillains] = useState([])
+  const [students, setStudents] = useState([]);
+  const [proHeroes, setProHeroes] = useState([]);
+  const [villains, setVillains] = useState([]);
 
   useEffect(() => {
     const gatherStudents = async () => {
-      const students = await axios("https://myheroacademiaapi.com/api/character?occupation=student")
-      console.log(students.data.result)
-    }
-    gatherStudents()
-  }, [students])
-  
+      const students = await axios(
+        "https://myheroacademiaapi.com/api/character?occupation=student"
+      );
+      console.log(students.data.result);
+    };
+    gatherStudents();
+  }, [students]);
+
   useEffect(() => {
     const gatherHeroes = async () => {
-      const proHero = await axios("https://myheroacademiaapi.com/api/character?occupation=hero")
-      console.log(proHero.data.result)
-    }
-    gatherHeroes()
-  }, [proHeroes])
-  
+      const proHero = await axios(
+        "https://myheroacademiaapi.com/api/character?occupation=hero"
+      );
+      console.log(proHero.data.result);
+    };
+    gatherHeroes();
+  }, [proHeroes]);
+
   useEffect(() => {
     const leagueOfVillains = async () => {
-      const leagueOfV = await axios("https://myheroacademiaapi.com/api/character?occupation=villain")
-      console.log(leagueOfV.data.result)
-    }
-    leagueOfVillains()
-  },[villains])
-  
+      const leagueOfV = await axios(
+        "https://myheroacademiaapi.com/api/character?occupation=villain"
+      );
+      console.log(leagueOfV.data.result);
+    };
+    leagueOfVillains();
+  }, [villains]);
+
   return (
     <div className="App">
-      <Route exact='/'>
+      <Route path="/" exact>
         <Welcome />
       </Route>
-      <h1>Plus Ultra! </h1>
-      </div>
+      <Route path='/plus' exact>
+        <h1>Plus Ultra! </h1>
+      </Route>
+    </div>
   );
 }
 
--- src/Screens/Welcome/Welcome.jsx
+++ src/Screens/Welcome/Welcome.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
-
+import React from "react";
+import "./Welcome.css";
 
 export default function Welcome() {
-  return ( 
+  return (
     <>
-      <div className='title'>
-        <h2>I am here!</h2>
+      <div className="title">
+        <div className="boxed">
+          <h2 className="plus">Plus Ultra!</h2>
+        </div>
       </div>
-      </>
-  )
+      <div className='deku'>
+      <img src="https://i.ibb.co/T2hzf8y/dekuwriting.gif"/>
+      </div>
+      <div className='description'>
+        <p>A fun project involving MHA and its API. Take a look at some character stats.</p>
+        <p>Note there are some light spoilers here. If you spoil yourself, you are responsible so don't blame me! :)</p>
+      </div>
+    </>
+  );
 }
