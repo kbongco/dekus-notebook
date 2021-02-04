019932e0d9c0a5ae41e851d2f31591e6ad6c28d2
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2021-02-03 19:24:55 -0500

    test commit villains

--- src/Screens/VillainPage/Villains.jsx
+++ src/Screens/VillainPage/Villains.jsx
@@ -3,7 +3,7 @@ import Pagination from 'react-js-pagination'
 
 export default function Villains(props) {
   const { villains } = props;
-  console.log(props)
+  console.log(props);
   return (
     <h1>Derp</h1>
   )
