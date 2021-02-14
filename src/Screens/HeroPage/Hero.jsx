81bfaa33e8959a834fe92bf996b819b9f6e81a09
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2021-02-13 19:23:46 -0500

    added cards and modal information, will need to experiment more

--- src/Screens/HeroPage/Hero.jsx
+++ src/Screens/HeroPage/Hero.jsx
@@ -2,10 +2,22 @@ import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import CardDeck from "react-bootstrap/CardDeck";
 import Button from "react-bootstrap/Button";
+import Modal from 'react-bootstrap/Modal'
 
 export default function Hero(props) {
   const { proHeroes } = props;
   console.log(props);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const showModal = () => {
+    setIsOpen(true);
+  }
+
+  const hideModal = () => {
+    setIsOpen(false);
+  }
+
   return (
     <div className="all-students">
       <h1>Heroes </h1>
@@ -20,11 +32,23 @@ export default function Hero(props) {
             <Card.Body>
               <Card.Title>{proHero.name}</Card.Title>
               <Card.Text>{proHero.quirk}</Card.Text>
-              <Button variant="primary">More Information</Button>
+              <Button variant="primary" onClick={showModal}>More Information</Button>
             </Card.Body>
           </Card>
         ))}
       </CardDeck>
+
+      {proHeroes.map((proHero) => (
+        <Modal show={isOpen}>
+          <Modal.Header closeButton>
+            <Modal.Title>{proHero.name}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>{proHero.description}</p>
+          </Modal.Body>
+        </Modal>
+      ))}
+
     </div>
   );
 }
